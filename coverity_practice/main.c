9e2a9993d7fcf5c949c683fd616839cccbc3b954
Peng Ching-Ting <tony79216@gmail.com>
2021-05-30 09:33:13 +0800

    Initial Commit

---
+++ coverity_practice/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  coverity_practice
+//
+//  Created by 彭敬庭 on 5/30/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
